738245af33fb8a0abba4e43ed17fee301b57b2a7
Acceleration123 <1497929914@qq.com>
2025-05-08 18:54:02 +0800

    Conventional SRP Algorithm using DS Beam for SSL

---
+++ SRP.py
@@ -0,0 +1,119 @@
+import torch
+import torch.nn as nn
+import soundfile as sf
+import numpy as np
+
+from scipy import signal
+from tqdm import tqdm
+from einops import rearrange
+
+
+class SRP(nn.Module):
+    """
+    Steering Response Power (SRP) For DOA Estimation Exploiting Uniform Linear Array (ULA)
+
+    Delay and Sum (DS) Beam
+    """
+    def __init__(
+            self,
+            d_inter,
+            c=343,
+            n_fft=512,
+            hop_length=256,
+            window_length=512,
+            fs=16000
+    ):
+        super(SRP, self).__init__()
+        self.d_inter = d_inter
+        self.c = c
+        self.n_fft = n_fft
+        self.hop_length = hop_length
+        self.window = torch.hann_window(window_length).pow(0.5)
+        self.fs = fs
+
+    def multi_channel_stft(self, x):
+        """
+        input: x: (T (n_samples), C (channels))
+        output: X: (C, F, T) complex valued
+                f_analog: (F - 1,) except f = 0
+        """
+        device = x.device
+        x = x.unsqueeze(0)  # (1, T, C)
+        bs = x.shape[0]
+        x = x.transpose(1, 2).reshape(-1, x.size(1))
+
+        X = torch.stft(x, n_fft=self.n_fft, hop_length=self.hop_length, window=self.window.to(device),
+                       onesided=True, return_complex=True)
+
+        X = torch.view_as_real(X)  # (1, F, T, 2)
+        X = X.view(bs, -1, X.shape[1], X.shape[2], 2)  # (1, C, F, T, 2)
+
+        X = torch.complex(X[..., 0], X[..., 1])  # (1, C, F, T)
+        f_analog = torch.linspace(0, self.fs / 2, self.n_fft // 2 + 1)
+
+        return X.squeeze(0), f_analog[1:]
+
+    def srp_ula(self, sig_stft, f_analog):
+        """
+        sig_stft: (C, F, T)
+        f_analog: (F - 1,) except f = 0
+        """
+        array_num = sig_stft.shape[0]
+        theta = torch.linspace(-90, 90, 361)
+        theta = torch.deg2rad(theta)
+        theta_sin = torch.sin(theta)
+
+        sig_stft = rearrange(sig_stft, 'c f t -> t f c')  # (T, F, C)
+        power_map = torch.zeros(len(f_analog), len(theta))  # (F, angle)
+
+        for i in tqdm(range(len(f_analog))):
+            f = f_analog[i]
+            for j in range(len(theta)):
+                phase = (2 * torch.pi * f * theta_sin[j] * self.d_inter *
+                         torch.linspace(0, array_num - 1, array_num) / self.c)  # (C,)
+                steer_vec = torch.exp(-1j * phase)  # (C,)
+                sig_mic = sig_stft[:, i + 1, :]  # (T, C)
+                response_complex = torch.matmul(sig_mic.unsqueeze(1), steer_vec.unsqueeze(1))
+                response_norm = torch.mean(torch.abs(response_complex).squeeze(1))
+                power_map[i, j] = response_norm ** 2
+
+        power_map_mean = torch.mean(power_map, dim=0)  # (angle,)
+        doa_order = torch.argmax(power_map_mean)
+
+        return doa_order * 0.5 - 90
+
+    def forward(self, x):
+        """
+        input: x: (T (n_samples), C (channels))
+        output: doa_estimation: (1,)
+        """
+        sig_stft, f_analog = self.multi_channel_stft(x)
+        doa_est = self.srp_ula(sig_stft, f_analog)
+
+        return doa_est
+
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    speech, fs = sf.read('00001.wav', dtype='float32')
+    rir = np.load('0003_rt60=0.44_d=0.020_r1=2.00_phi1=-55.01_r2=2.00_phi2=78.76.npy')
+
+    sensor_1 = signal.fftconvolve(speech, rir[0][1], mode='same')
+    sensor_2 = signal.fftconvolve(speech, rir[1][1], mode='same')
+    sensor_3 = signal.fftconvolve(speech, rir[2][1], mode='same')
+    sensor_4 = signal.fftconvolve(speech, rir[3][1], mode='same')
+
+    sig = np.stack([sensor_1, sensor_2, sensor_3, sensor_4], axis=1)  # (T, 4)
+    sig = torch.tensor(sig).to(device)
+
+    srp_func = SRP(d_inter=0.02)
+    doa_est = srp_func(sig)
+    print(doa_est)
+
+
+
+
+
+
+
+
